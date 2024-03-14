a089f824c77957368e647cac56cbc898729d9172
Playdata <chelirt13@naver.com>
2024-03-14 16:35:11 +0900

    .

--- src/main/java/com/example/travelproject/model/repository/BoardRepository.java
+++ src/main/java/com/example/travelproject/model/repository/BoardRepository.java
@@ -3,7 +3,10 @@ package com.example.travelproject.model.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.travelproject.model.entity.BoardEntity;
-public interface BoardRepository extends JpaRepository<BoardEntity, Long>{
-    public BoardEntity findByNoticeId(long noticeId);
 
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    public BoardEntity selectByNotice(long noticeId);
+
+    // @Query(value = "select * from Notice order by desc", nativeQuery = true)
+    // public BoardEntity showNotice();
 }
